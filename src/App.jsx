7fe7de93064e2ffd29fef64ebc7c1736ebfa0421
achraf-bogryn <achrafbogryn12345@gmail.com>
2025-06-29 23:17:30 +0100

    call components in root components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import Hero from './sections/Hero'
+import Projects from './sections/Projects'
 
 const App = () => {
   return (
     <>
       {/* Header section */}
       <Header />
-
-      
+{/* Hero section */}
+      <Hero />
+      {/* projects section */}
+      <Projects />
     </>
 
   )
