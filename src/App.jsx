053dc9b1d1ed5eba187f42f8e1f8d8173de06fa8
achraf-bogryn <achrafbogryn12345@gmail.com>
2025-06-30 05:36:56 +0100

    call skills components inside app

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import Header from './components/Header'
 import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
 import Projects from './sections/Projects'
+import About from './sections/About'
+import Skills from './sections/Skills'
 
 const App = () => {
   return (
     <>
       {/* Header section */}
       <Header />
-{/* Hero section */}
+      {/* Hero section */}
       <Hero />
       {/* projects section */}
       <Projects />
+       {/* about section */}
+       <About />
+       {/* Skills */}
+       <Skills />
     </>
 
   )
