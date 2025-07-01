80dbe622754f49d168a25af2ee307b96dc3e728e
achraf-bogryn <achrafbogryn12345@gmail.com>
2025-07-01 06:56:16 +0100

    do some update in projects array

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,6 +2,9 @@
 import Project1 from '../assets/project-1.png';
 import Project3 from '../assets/project-2.png';
 import Project2 from '../assets/project-3.png';
+import Project4 from '../assets/Churn .PNG';
+import Project5 from '../assets/sentiment.PNG';
+import Project6 from '../assets/next.PNG';
 
 // skills images
 import Html from '../assets/html5.svg';
@@ -33,41 +36,7 @@ import aws from '../assets/aws.svg';
 import gcp from '../assets/gcp.svg';
 import java from '../assets/java.svg';
 import bi from '../assets/bi.svg';
-// export const projects = [
-//   {
-//     id: 1,
-//     title: 'Zen',
-//     description:
-//       'Zen is a modern hotel website built with Next.js, featuring a sleek design powered by Tailwind CSS. It offers an intuitive user interface to explore rooms, amenities, and make reservations seamlessly.',
-//     image: Project1,
-//     stack: ['React', 'Next.js', 'Tailwind CSS'],
-//     liveUrl: 'https://www.google.com',
-//     sourceUrl: 'https://www.google.com',
-//   },
 
-//   {
-//     id: 2,
-//     title: 'Pixar',
-//     description:
-//       'Pixar is a website clone built with React, featuring a responsive design, carousels, and a navigation system. The website is a perfect example of how to create a modern website with a clean and minimalistic design.',
-
-//     image: Project2,
-//     stack: ['React', 'Tailwind CSS', 'JavaScript'],
-//     liveUrl: 'https://www.google.com',
-//     sourceUrl: 'https://www.google.com',
-//   },
-
-//   {
-//     id: 3,
-//     title: 'Furniq',
-//     description:
-//       'This project is a modern furniture e-commerce website with a clean and minimalistic design. The website is built with React and features a responsive design, a functional shopping cart, and a user-friendly navigation system.',
-//     image: Project3,
-//     stack: ['React', 'Tailwind CSS', 'JavaScript'],
-//     liveUrl: 'https://www.google.com',
-//     sourceUrl: 'https://www.google.com',
-//   },
-// ];
 
 export const projects = [
   {
@@ -103,16 +72,16 @@ export const projects = [
     sourceUrl: 'https://github.com/achraf-bogryn/LSTM-NextWordGen/tree/main',
   },
 
-  {
-    id: 4,
-    title: 'Algerian Forest Fires Regression',
-    description:
-      'Built a regression model using various ML techniques to predict forest fires in Algeria based on meteorological and environmental data.',
-    image: Project3,
-    stack: ['Scikit-learn', 'Pandas', 'NumPy', 'Matplotlib', 'Seaborn', 'Regression'],
-    liveUrl: '',
-    sourceUrl: 'https://github.com/achraf-bogryn/algerian-forest-fires-regression/tree/main',
-  },
+  // {
+  //   id: 4,
+  //   title: 'Algerian Forest Fires Regression',
+  //   description:
+  //     'Built a regression model using various ML techniques to predict forest fires in Algeria based on meteorological and environmental data.',
+  //   image: Project3,
+  //   stack: ['Scikit-learn', 'Pandas', 'NumPy', 'Matplotlib', 'Seaborn', 'Regression'],
+  //   liveUrl: '',
+  //   sourceUrl: 'https://github.com/achraf-bogryn/algerian-forest-fires-regression/tree/main',
+  // },
 ];
 
 
