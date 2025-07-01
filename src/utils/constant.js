c1d39d456c7cd96c24f5900d1ce2576438d9dad1
achraf-bogryn <achrafbogryn12345@gmail.com>
2025-07-01 03:27:20 +0100

    update in array that contain skills

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -12,7 +12,27 @@ import Node from '../assets/nodejs.svg';
 import TypeScript from '../assets/typescript.svg';
 import Tailwind from '../assets/tailwindcss.svg';
 import Postgres from '../assets/postgresql.svg';
-
+import python from '../assets/python.svg';
+import pandas from '../assets/pandas.svg';
+import numpy from '../assets/numpy.svg';
+import scikit from '../assets/scikitlearn.svg';
+import tensorflow from '../assets/tensorflow.svg';
+import pytorch from '../assets/pytorch.svg';
+import flask from '../assets/flask.svg';
+import spring from '../assets/spring.svg';
+import next from '../assets/next.svg';
+import spark from '../assets/spark.svg';
+import hadoop from '../assets/hadoop.svg';
+import airflow from '../assets/airflow.svg';
+import kafka from '../assets/kafka.svg';
+import sql from '../assets/sql.svg';
+import mongodb from '../assets/mongodb.svg';
+import docker from '../assets/docker.svg';
+import git from '../assets/git.svg';
+import aws from '../assets/aws.svg';
+import gcp from '../assets/gcp.svg';
+import java from '../assets/java.svg';
+import bi from '../assets/bi.svg';
 // export const projects = [
 //   {
 //     id: 1,
@@ -97,51 +117,48 @@ export const projects = [
 
 
 export const skills = [
-  {
-    id: 1,
-    name: 'HTML',
-    icon: Html,
-  },
+  // 🟦 Programming Languages
+  { id: 2, name: 'Python', icon: python },
+  { id: 1, name: 'Java', icon: java },
+  { id: 3, name: 'JavaScript', icon: Js },
 
-  {
-    id: 2,
-    name: 'CSS',
-    icon: Css,
-  },
+    // 🔥 Big Data & Pipelines
+  { id: 18, name: 'Spark', icon: spark },
+  { id: 19, name: 'Hadoop', icon: hadoop },
+  { id: 20, name: 'Airflow', icon: airflow },
+  { id: 21, name: 'Kafka', icon: kafka },
 
-  {
-    id: 3,
-    name: 'JavaScript',
-    icon: Js,
-  },
 
-  {
-    id: 4,
-    name: 'React',
-    icon: React,
-  },
+    // 🧠 Data Science Libraries
+  { id: 9, name: 'Powe BI', icon: bi },
+  { id: 10, name: 'Pandas', icon: pandas },
+  { id: 11, name: 'NumPy', icon: numpy },
+  { id: 12, name: 'Scikit-learn', icon: scikit },
+  { id: 13, name: 'TensorFlow', icon: tensorflow },
+  { id: 14, name: 'PyTorch', icon: pytorch },
 
-  {
-    id: 5,
-    name: 'Node',
-    icon: Node,
-  },
+  // 🟩 Frontend & Frameworks
+  { id: 4, name: 'React', icon: React },
+  { id: 17, name: 'Next.js', icon: next },
 
-  {
-    id: 6,
-    name: 'TypeScript',
-    icon: TypeScript,
-  },
 
-  {
-    id: 7,
-    name: 'Tailwind',
-    icon: Tailwind,
-  },
 
-  {
-    id: 8,
-    name: 'Postgres',
-    icon: Postgres,
-  },
+  // ⚙️ Backend & APIs
+  { id: 15, name: 'Flask', icon: flask },
+  { id: 16, name: 'Spring', icon: spring },
+
+
+
+  // 🗄️ Databases
+  { id: 22, name: 'SQL', icon: sql },
+  { id: 23, name: 'MongoDB', icon: mongodb },
+
+  // 🚀 Cloud & DevOps
+  { id: 24, name: 'Docker', icon: docker },
+  { id: 26, name: 'AWS', icon: aws },
+  { id: 27, name: 'GCP', icon: gcp },
+
+  // 🔧 Version Control
+  { id: 25, name: 'Git', icon: git },
 ];
+
