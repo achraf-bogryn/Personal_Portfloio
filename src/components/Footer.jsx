16e47140c53b40c9659065a73012baaae50f666a
achraf-bogryn <achrafbogryn12345@gmail.com>
2025-07-01 03:38:02 +0100

    footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+     return (
+          <footer className='pt-32 pb-6 text-center'>
+               <div className='container'>
+                    <p className='mb-2'>Copyright @ Achraf Bogryn {new Date().getFullYear()}. All right</p>
+               </div>
+          </footer>
+     )
+}
+
+export default Footer
