36217a711772222a72d665cb8ab6b6ec536f57a0
achraf-bogryn <achrafbogryn12345@gmail.com>
2025-07-01 06:36:46 +0100

    remove linkdin and github links from about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -25,14 +25,14 @@ const About = () => {
                               <p className='leading-7 '>
                                    Hi, I’m Achraf Bogryn, a junior data scientist with a Master’s degree in Data Science and Engineering. I’m currently deepening my expertise in machine learning, big data technologies, and data analysis by working on personal projects and staying up-to-date with the latest industry trends. I’m actively seeking internships or entry-level roles where I can apply my skills to solve real-world problems, collaborate with experienced teams, and contribute to data-driven decision making. My goal is to grow as a data professional by continuously learning and tackling impactful challenges in the field.
                               </p>
-                              <div className='flex gap-4 mt-4'>
+                              {/* <div className='flex gap-4 mt-4'>
                                    <a href='https://github.com/achraf-bogryn' target='_blank' rel='noopener noreferrer'>
                                         <FaGithub size={32} className='hover:text-blue-600 transition-colors' />
                                    </a>
                                    <a href='https://www.linkedin.com/in/achraf-bogryn/' target='_blank' rel='noopener noreferrer'>
                                         <FaLinkedin size={32} className='hover:text-blue-600 transition-colors' />
                                    </a>
-                              </div>
+                              </div> */}
                          </div>
                     </div>
                </div>
